86e518adbf53ebe8088b8eb243554ceea5cbbadd
cheeseerhino8 <58511837+cheesethino8@users.noreply.github.com>
2020-07-12 17:41:53 +0300

    cleanup & comments
    heard that comments were useful when you're trying to read it later

--- PWGen/main.py
+++ PWGen/main.py
@@ -1,44 +1,37 @@
 import random
 import string
-from random import randrange
+import os
 
-pwLength = input("Enter password length:")
-pwLength = int(pwLength)
+pwLength = input("Enter password length:") #asks user for an int to be used as the length of the password
+pwLength = int(pwLength) #converts user input to an integer
 
-digChoice = input("Digits? (Y/N)")
-letChoice = input("Letters? (Y/N)")
-symChoice = input("Symbols? (Y/N)")
+digChoice = input("Digits? (Y/N)") #asks if user wants digits in generated passwords
+letChoice = input("Letters? (Y/N)") #asks if user wants letters in generated passwords
+symChoice = input("Symbols? (Y/N)")#asks if user wants symbols in generated passwords
 
-#print(digChoice + letChoice + symChoice)
 
-digChoice = "y" in digChoice or "Y" in digChoice
-letChoice = "y" in letChoice or "Y" in letChoice
-symChoice = "y" in symChoice or "Y" in symChoice
-
-#print(str(digChoice) + str(letChoice) + str(symChoice))
+digChoice = "y" in digChoice or "Y" in digChoice # convert answer to boolean
+letChoice = "y" in letChoice or "Y" in letChoice # convert answer to boolean
+symChoice = "y" in symChoice or "Y" in symChoice # convert answer to boolean
 
 global n
-n = pwLength
+n = pwLength #set n to equal to pwlength
 global passwd
-passwd = ""
+passwd = "" #creates empty variable for passwd
 
-def generatePassword(n):
+def generatePassword(n): #recrusive function
     global passwd, result
 
-    if (n <= 0):
-        print(passwd)
-        result = 0
-        return result
-    rndNum = random.randint(0,2)
+    rndNum = random.randint(0,2) #create random number between 0 & 2, used to randomize password
 
-    if (digChoice == True and rndNum == 0):
-        digPasswd = "".join({random.choice(string.digits)})
-        passwd = passwd + digPasswd
-        n = n - 1
-        if (n == 0):
+    if (digChoice == True and rndNum == 0): #checks if numbers are wanted for password
+        digPasswd = "".join({random.choice(string.digits)}) #creates a random number
+        passwd = passwd + digPasswd #adds previously created number to the end of the password
+        n = n - 1 #increment n
+        if (n == 0): #if n = 0, then print resulting password & exit
             print(passwd)
             return 0
-
+                #above comments are basically the same for the 2 loops below(letters & symbols)
     if (letChoice == True and rndNum == 1):
         letPasswd = "".join({random.choice(string.ascii_letters)})
         passwd = passwd + letPasswd
@@ -54,8 +47,7 @@ def generatePassword(n):
         if (n == 0):
             print(passwd)
             return 0
-
     generatePassword(n)
 
-print(passwd)
-generatePassword(n)
+generatePassword(n) #runs recursive function
+os.system("pause") #creates a pause so people can actually copy the damn result
